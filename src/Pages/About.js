ee5314e12bf437e6ef2f92b64584d823aa6bfb86
Ashwin Chatterji <ashwinath@hotmail.com>
2017-07-08 21:33:40 +0800

    removed pagesection, use a css instead. added about summary

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -5,10 +5,53 @@ class About extends Component {
   render() {
     return (
       <SubPage pageName="About Me">
-        <h1>Lorem Ipsum</h1>
+        <Summary/>
       </SubPage>
     );
   }
+}
+
+function gotoWebsite(event) {
+  window.location = icons[event.target.id].website;
+}
+
+const icons = {
+  react: {
+    icon: "devicon-react-original",
+    desc: "Modern frontend web development.",
+    website: "https://facebook.github.io/react/"
+  },
+  nodejs: {
+    icon: "devicon-nodejs-plain",
+    desc: "Build highly concurrent apps with Node.",
+    website: "https://nodejs.org/en/"
+  },
+  java: {
+    icon: "devicon-java-plain",
+    desc: "Enterprise software development.",
+    website: "https://www.java.com/en/"
+  }
+};
+
+function Summary() {
+  return (
+    <div className="section-band-dark text-center">
+      {Object.entries(icons).map(([type, value]) => {
+        // Please note that the background colours per icon
+        return (
+          <div key={type} className="col-md-4 icon-pad">
+            <i id={type}
+              onClick={gotoWebsite}
+              className={`icon ${value.icon}`}/>
+            <p 
+              className="about-summary-desc">
+              {value.desc}
+            </p>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default About;
