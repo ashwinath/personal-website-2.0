cb9eef07feeb81673fc432ebba5042a418c27873
Ashwin Chatterji <ashwinath@hotmail.com>
2017-07-09 22:08:36 +0800

    build before running test

--- test/server.test.js
+++ test/server.test.js
@@ -4,16 +4,6 @@ const exec = require('mz/child_process').exec,
       expect = require('chai').expect,
       app = require('../server/server');
 
-describe('builds application', function () {
-  it('builds to "build" directory', function () {
-    // Disable mocha time-out because this takes a lot of time
-    this.timeout(0);
-
-    // Run process
-    return exec('yarn build');
-  });
-});
-
 describe('express serving', function () {
   it('responds to / with the index.html', function () {
     return request(app)
