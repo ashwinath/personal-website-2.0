b7347a2f06e996796ae37a188363a9e202f2f6f7
Ashwin Chatterji <ashwinath@hotmail.com>
2017-07-11 21:03:19 +0800

    first attempt at automated deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-yarn build;
-scp -r build/ ashwin@128.199.203.199:/opt/apps/personal-website-2.0
-ssh -i ~/.ssh/id_rsa ashwin@128.199.203.199 'cd /opt/apps/personal-website-2.0; git pull; yarn; pm2 restart server';
+scp -r $TRAVIS_BUILD_DIR/build/ ashwin@128.199.203.199:/opt/apps/personal-website-2.0
+ssh -i /temp/id_rsa ashwin@128.199.203.199 'cd /opt/apps/personal-website-2.0; git pull; yarn; pm2 restart server';
