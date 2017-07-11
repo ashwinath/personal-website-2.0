a6c0ece4e7e90c3cc9b9e99dcb33a75256e5137d
Ashwin Chatterji <ashwinath@hotmail.com>
2017-07-11 21:27:42 +0800

    remoed travis build path see if it works

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp -r $TRAVIS_BUILD_DIR/build/ ashwin@128.199.203.199:/opt/apps/personal-website-2.0
+scp -r build/ ashwin@128.199.203.199:/opt/apps/personal-website-2.0
 ssh -i /temp/id_rsa ashwin@128.199.203.199 'cd /opt/apps/personal-website-2.0; git pull; yarn; pm2 restart server';
