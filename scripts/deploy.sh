6f800d6a70ad75c274092196eaaa2bd5d070b5d7
Ashwin Chatterji <ashwinath@hotmail.com>
2017-07-11 21:42:19 +0800

    still investigating why build was not scp-ed in

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-scp -r build/ ashwin@128.199.203.199:/opt/apps/personal-website-2.0
-ssh -i /temp/id_rsa ashwin@128.199.203.199 'cd /opt/apps/personal-website-2.0; git pull; yarn; pm2 restart server';
+ls;
+scp -r build/ ashwin@128.199.203.199:/opt/apps/personal-website-2.0;
+ssh  ashwin@128.199.203.199 'cd /opt/apps/personal-website-2.0; git pull; yarn; pm2 restart server';
