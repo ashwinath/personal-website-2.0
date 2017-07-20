6a5c9b4d4aa3121443beec1d81516bbbef9f2ecf
Ashwin Chatterji <ashwinath@hotmail.com>
2017-07-20 20:56:26 +0800

    morgan should take in real ip address instead of reverse proxy's ip

--- server/server.js
+++ server/server.js
@@ -3,8 +3,14 @@ const express = require('express'),
       path = require('path'),
       morgan = require('morgan');
 
+// Morgan Freeman is supposed to take in IP from x-headers and not localhost
+morgan.token('remote-addr', req => {
+  return req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+});
+
 // We love Morgan Freeman
 app.use(morgan('combined'));
+
 
 // Serve static react files
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
