e0a2f5743da3a6a00225ab8fba5b3a819c4a95e2
Ashwin Chatterji <ashwinath@hotmail.com>
2017-07-23 17:06:48 +0800

    added dynamic checking whenever each character is set

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -4,65 +4,49 @@ import isEmail from 'validator/lib/isEmail';
 import isNumeric from 'validator/lib/isNumeric';
 
 class Contact extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      nameError: true,
-      emailError: true,
-      contactNoError: true,
-      messageError: true,
-      validated: false
+      name: "",
+      nameValid: true,
+      email: "",
+      emailValid: true,
+      contactNo: "",
+      contactNoValid: true,
+      content: "",
+      contentValid: true
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    let name = event.target.name.value;
-    let email = event.target.email.value;
-    let contactNo = event.target.contactNo.value;
-    let content = event.target.content.value;
 
-    this.setState(() => {
-      let newState = this.validateContactForm({
-        name: name,
-        email: email,
-        contactNo: contactNo,
-        content: content
-      });
+    // Final check, in case the user did not touch the keys
+    let valid = this.state.name 
+      && this.state.email 
+      && this.state.content 
+      && this.state.nameValid
+      && this.state.emailValid
+      && this.state.contactNoValid
+      && this.state.contentValid
+      ? true : false;
 
-      console.log(newState)
-
-      let okToSubmit = Object.entries(newState).reduce((acc, pair) => {
-        // eslint-disable-next-line
-        const [key, value] = pair;
-        return acc && value;
-      }, true);
-
-      if (okToSubmit) {
-        // TODO: send post request here
-        console.log("validated!")
-        // TODO: render method shld show a success
-      } else {
-        // TODO: show validation
-        // do nothing here, render method should handle
-        console.log("failed validation")
-      }
-
-      newState.validated = true;
-      return newState;
-    });
-
+    // TODO: handle post
+    console.log(`Verification result was ${valid}`);
   }
 
+  // TODO: shld validate on the fly
   validateContactForm(contactForm) {
     const { name, email, contactNo, content } = contactForm;
     const validEmail = isEmail(email);
     const validContact = contactNo ? isNumeric(contactNo) : true;
 
     return {
+      name: name,
       nameError: name ? true : false,
       emailError: validEmail,
       contactNoError: validContact,
@@ -70,7 +54,34 @@ class Contact extends Component {
     }
   }
 
-  validateField(string) {
+  validate(name, value) {
+    if (name === 'email') {
+      return isEmail(value)
+    } else if (name === 'name' || name === 'content') {
+      return value ? true : false;
+    } else if (name === 'contactNo') {
+      return value ? isNumeric(value) : true;
+    }
+  }
+
+  handleChange(event) {
+    const {target} = event;
+
+    // new inputs
+    const name = target.name;
+    const value = target.value;
+
+    // old values, we only change the old ones
+    let newState = this.state;
+    newState[name] = value;
+
+    // Validation
+    const valid = this.validate(name, value);
+    newState[`${name}Valid`] = valid;
+
+    this.setState(() => {
+      return newState;
+    });
   }
 
   render() {
@@ -78,45 +89,8 @@ class Contact extends Component {
       <SubPage pageName="Contact Me">
         <form onSubmit={this.handleSubmit}
           className="col-md-offset-1 col-md-10 contact-form">
-
-          <div className="col-md-6">
-            <div className="form-group">
-              <input
-                name="name"
-                type="text"
-                className="form-control"
-                placeholder="Name"
-                aria-describedby="sizing-addon1"/>
-            </div>
-
-            <div className="form-group">
-              <input
-                name="email"
-                type="text"
-                className="form-control"
-                placeholder="E-Mail"
-                aria-describedby="sizing-addon1"/>
-            </div>
-
-            <div className="form-group">
-              <input
-                name="contactNo"
-                type="text"
-                className="form-control"
-                placeholder="Contact Number"
-                aria-describedby="sizing-addon1"/>
-            </div>
-          </div>
-
-          <div className="col-md-6">
-            <div className="form-group">
-              <textarea
-                name="content"
-                className="form-control"
-                placeholder="Message"/>
-            </div>
-          </div>
-
+          <ContactParticulars state={this.state} handleChange={this.handleChange}/>
+          <ContactMessage state={this.state} handleChange={this.handleChange}/>
           <button type="submit" 
             value="submit"
             className="btn btn-lg submit-btn">
@@ -125,6 +99,61 @@ class Contact extends Component {
       </SubPage>
     );
   }
+}
+
+function ContactMessage(props) {
+  return (
+    <div className="col-md-6">
+      <div className="form-group">
+        <textarea
+          name="content"
+          value={props.state.content}
+          onChange={props.handleChange}
+          className="form-control"
+          placeholder="Message"/>
+      </div>
+    </div>
+  );
+}
+
+function ContactParticulars(props) {
+  return (
+    <div className="col-md-6">
+      <div className="form-group">
+        <input
+          name="name"
+          value={props.state.name}
+          onChange={props.handleChange}
+          type="text"
+          className="form-control"
+          placeholder="Name"
+          aria-describedby="sizing-addon1"/>
+      </div>
+
+      <div className="form-group">
+        <input
+          name="email"
+          value={props.state.email}
+          onChange={props.handleChange}
+          type="text"
+          className="form-control"
+          placeholder="E-Mail"
+          aria-describedby="sizing-addon1"/>
+      </div>
+
+      <div className="form-group">
+        <input
+          name="contactNo"
+          value={props.state.contactNo}
+          onChange={props.handleChange}
+          type="text"
+          className="form-control"
+          placeholder="Contact Number"
+          aria-describedby="sizing-addon1"/>
+      </div>
+    </div>
+  );
+
 }
 
 export default Contact;
