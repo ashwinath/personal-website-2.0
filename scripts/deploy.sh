e98f2dbafe3e4530299c59e9c68b5d7a779f1aac
Ashwin Chatterji <ashwinath@hotmail.com>
2017-07-28 21:40:58 +0800

    updated build script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+yarn build;
 rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress build/ ashwin@128.199.203.199:/opt/apps/personal-website-2.0/build;
 ssh ashwin@128.199.203.199 'cd /opt/apps/personal-website-2.0; git pull; yarn; source ~/.zshrc; pm2 restart server';
